f886fa221fea1c8874fdfabbd47bd53518a10829
Grigory <grigorydavydenko@yandex.ru>
2021-04-23 18:15:42 +0700

    School schedule table

--- netshool/test.py
+++ netshool/test.py
@@ -1,3 +1,62 @@
-a = ('Математика', 'Физика', 'Химия', 'Информатика', 'Русский язык', 'Английский язык', 'История России',
-     'Всеобщая история', 'Обществознание', 'Биология', 'География', 'Литература', 'Физкультура', 'ОБЖ',
-     'Технология', 'ИЗО', 'Музыка')
+import flask
+from flask import jsonify, request
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_jobs():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('id', 'team_leader', 'job', 'work_size', 'collaborators',
+                                    'start_date', 'end_date', 'is_finished',
+                                    'user.name'))
+                 for item in jobs]
+        }
+    )
+
+
+@blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
+def get_one_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+    if not job:
+        return jsonify({'error': 'Not found'})
+    return jsonify(
+        {
+            'job': job.to_dict(only=('id', 'team_leader', 'job', 'work_size', 'collaborators',
+                                     'start_date', 'end_date', 'is_finished',
+                                     'user.name'))
+        }
+    )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['id', 'team_leader', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    job = Jobs(
+        id=request.json['id'],
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
