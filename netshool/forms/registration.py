e9f25e3a100f57b35e76ee03ae55a8d3ffcff9c6
Grigory <grigorydavydenko@yandex.ru>
2022-03-06 17:27:39 +0700

    requirements try 5 all update

--- netshool/forms/registration.py
+++ netshool/forms/registration.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, IntegerField, BooleanField
 from wtforms import SelectField, widgets, SelectMultipleField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 from wtforms.validators import DataRequired
 
 
